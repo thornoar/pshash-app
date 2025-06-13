91436665d0d08307656b846db1fa9955140a1af0
Roman Maksimovich <r.a.maksimovich@gmail.com>
2025-06-14 07:41:07 +0800

    auto

--- app/src/test/java/com/example/pshash/ExampleUnitTest.kt
+++ app/src/test/java/com/example/pshash/ExampleUnitTest.kt
@@ -4,8 +4,6 @@ import androidx.compose.ui.util.fastJoinToString
 import org.junit.Test
 
 import org.junit.Assert.*
-//import com.example.pshash.*
-import java.util.LinkedList
 
 fun string (
     l : List<Char>
@@ -17,22 +15,5 @@ class ExampleUnitTest {
     @Test
     fun test() {
         println(string(getHash(pinCodeConfiguration, tbi(12902 + 45), tbi(45))))
-//        assertEquals(
-//            string(chooseOrdered(Pair(sourceNumbers, 4), tbi(30))),
-//            "1840"
-//        )
-    }
-
-    @Test
-    fun testGetHash () {
-        assertEquals(
-            string(getHash(pinCodeConfiguration, tbi(13156), tbi(45))),
-            "0643"
-        )
-    }
-
-    @Test
-    fun testGetPassword () {
-        println(getPassword("anlong", "hahahahihi", "567-321", "78-43"))
     }
 }
